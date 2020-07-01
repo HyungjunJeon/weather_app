7b44883f428c48a7e64be1d470bf6ddd3f3a42ad
HyungjunJeon <maru105kr@gmail.com>
2020-07-01 19:42:42 +0900

    React Native: Background Gradient

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class extends React.Component {
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
     this.setState({
-      isloading: false,
+      isLoading: false,
       condition: weather[0].main,
       temp,
     });
@@ -41,8 +41,8 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isloading, temp, condition } = this.state;
-    return isloading ? (
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
       <Loading />
     ) : (
       <Weather temp={Math.round(temp)} condition={condition} />
--- Weather.js
+++ Weather.js
@@ -1,17 +1,89 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weather({ temp }) {
+const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#373B44", " #4286f4"],
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", " #66A6FF"],
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", " #005BEA"],
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", " #B9B6E5"],
+  },
+  Mist: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", " #D39D38"],
+  },
+  Smoke: {
+    iconName: "weather-fog",
+    gradient: ["#4DA0B0", " #D39D3B"],
+  },
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", " #D39D3B"],
+  },
+  Dust: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", " #D39D3B"],
+  },
+  Fog: {
+    iconName: "weather-fog",
+    gradient: ["#4DA0B0", " #D39D3B"],
+  },
+  Sand: {
+    iconName: "weather-fog",
+    gradient: ["#4DA0B0", " #D39D3B"],
+  },
+  Ash: {
+    iconName: "weather-fog",
+    gradient: ["#4DA0B0", " #D39D3B"],
+  },
+  Squall: {
+    iconName: "weather-fog",
+    gradient: ["#4DA0B0", " #D39D3B"],
+  },
+  Tornado: {
+    iconName: "weather-tornado",
+    gradient: ["#4DA0B0", " #D39D3B"],
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#FF7300", " #FEF253"],
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#4DA0B0", " #D39D38"],
+  },
+};
+
+export default function Weather({ temp, condition }) {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <StatusBar barStyle="light-content" />
       <View style={styles.halfContainer}>
-        <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
+        <MaterialCommunityIcons
+          size={96}
+          name={weatherOptions[condition].iconName}
+          color="white"
+        />
         <Text style={styles.temp}>{temp}°</Text>
       </View>
       <View style={styles.halfContainer}></View>
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -44,6 +116,7 @@ const styles = StyleSheet.create({
   },
   temp: {
     fontSize: 42,
+    color: "white",
   },
   halfContainer: {
     flex: 1,
